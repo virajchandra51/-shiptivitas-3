2b4610e9bd6925eb8f00bd4dc9e5fc3a98cb3454
Viraj Chandra <virajchandra51@gmail.com>
2022-11-16 02:53:08 +0530

    Task Completed

--- answer.sql
+++ answer.sql
@@ -2,12 +2,22 @@
 
 -- PART 1: Create a SQL query that maps out the daily average users before and after the feature change
 
+select count(*) as Daily_Users, strftime('%m-%d-%Y', datetime(login_timestamp, 'unixepoch')) as Day from login_history group by strftime('%m-%d-%Y', datetime(login_timestamp, 'unixepoch')); 
+select avg(a.dayCount) from (select count(*) as dayCount from login_history group by strftime('%m-%d-%Y', datetime(login_timestamp, 'unixepoch'))) a;
 
+
+--query of before the feature change
+select count(*) as Count, strftime('%m-%d-%Y', datetime(login_timestamp, 'unixepoch')) as Day from login_history where login_timestamp < 1527898041 group by strftime('%m-%d-%Y', datetime(login_timestamp, 'unixepoch'));
+select avg(a.dayCount) from (select count(*) as dayCount from login_history where login_timestamp < 1527898041 group by strftime('%m-%d-%Y', datetime(login_timestamp, 'unixepoch'))) a;
+
+
+--query of after the feature change
+select count(*) as Count, strftime('%m-%d-%Y', datetime(login_timestamp, 'unixepoch')) as Day from login_history where login_timestamp > 1527898040 group by strftime('%m-%d-%Y', datetime(login_timestamp, 'unixepoch'));
+select avg(a.dayCount) from (select count(*) as dayCount from login_history where login_timestamp > 1527898040 group by strftime('%m-%d-%Y', datetime(login_timestamp, 'unixepoch'))) a;
 
 
 -- PART 2: Create a SQL query that indicates the number of status changes by card
 
-
-
+select cardID, count(*) from card_change_history where oldStatus != newStatus group by cardID; 
 
 
